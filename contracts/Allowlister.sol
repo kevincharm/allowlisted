c7ced309b8e807798c44c7e226bd479a82b4d06c
Kevin Tjiam <kevin@kevincharm.com>
2022-04-23 23:41:11 +0200

    allow users to register explicitly for raffle

--- contracts/Allowlister.sol
+++ contracts/Allowlister.sol
@@ -1,145 +1,160 @@
-//SPDX-License-Identifier: Unlicense
+// SPDX-License-Identifier: MIT
 pragma solidity 0.8.10;
 
-import "hardhat/console.sol";
-import "lens-protocol/contracts/interfaces/IFollowNFT.sol";
-import "lens-protocol/contracts/interfaces/ILensHub.sol";
-import "lens-protocol/contracts/libraries/DataTypes.sol";
-import "@openzeppelin/contracts/token/ERC721/extensions/IERC721Enumerable.sol";
+import {IFollowNFT} from "lens-protocol/contracts/interfaces/IFollowNFT.sol";
+import {ILensHub} from "lens-protocol/contracts/interfaces/ILensHub.sol";
+import {DataTypes} from "lens-protocol/contracts/libraries/DataTypes.sol";
+import {IERC721Enumerable} from "@openzeppelin/contracts/token/ERC721/extensions/IERC721Enumerable.sol";
 import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
 import {IRandomiserCallback} from "./IRandomiserCallback.sol";
 import {Randomiser} from "./Randomiser.sol";
 import {IValidateModule} from "./IValidateModule.sol";
 import {IWinnersModule} from "./IWinnersModule.sol";
 
+/**
+ * @title Allowlister
+ * @notice Contract that allows Lens users to register for an allowlist, then draws
+ *      n random winners for the allowlist.
+ */
 contract Allowlister is IRandomiserCallback, Ownable {
-    /// @dev Maximum invalid winner count per iteration before transaction exits.
-    uint256 public constant MAX_INVALID_COUNT = 10;
-
     /// @notice The LensHub contract
-    ILensHub public immutable s_lensHub;
+    ILensHub public immutable lensHub;
 
     /// @notice The `Randomiser` contract implementing the randomisation logic.
-    Randomiser immutable s_randomiser;
+    Randomiser public immutable randomiser;
 
     /// @notice Lens profile ID containing the list of followers to draw the raffle from.
-    uint256 immutable s_profileId;
+    uint256 public immutable raffleProfileId;
 
-    /// @notice Number of winners to draw from the raffle.
-    uint256 immutable s_numberToRaffle;
+    /// @notice Number of winners left to draw from the raffle.
+    uint256 public immutable winnersToDraw;
 
-    /// @dev Follower token IDs
-    uint256[] public ids;
+    /// @notice The contract implementing `IWinnersModule` that determines the logic
+    ///     for handling what happens with the winner addresses.
+    IWinnersModule public immutable winnersModule;
 
-    /// @notice Flag determining if raffle is already completed
-    bool public isRaffleFinished = false;
+    /// @notice The contract implementing `IValidateMOdule` that determines that logic
+    ///     for handling validation of each follower after they are drawn during the raffle.
+    IValidateModule public immutable validateModule;
+
+    /// @dev Profile IDs registered for raffle. This is the array from which random winners are picked.
+    uint256[] public s_registeredIds;
+
+    /// @dev Addresses corresponding to registered profile IDs.
+    mapping(uint256 => address) public s_registeredAddresses;
+
+    /// @dev Determines whether profile ID has already registered for raffle
+    mapping(uint256 => bool) public s_profileIdRegistered;
+
+    /// @dev Determines whether address has already registered for raffle
+    mapping(address => bool) public s_addressRegistered;
 
     /// @dev The current random number (changes every time a random number is requested)
     uint256 public s_randomState;
 
-    /// @notice The contract implementing `IWinnersModule` that determines the logic
-    ///     for handling what happens with the winner addresses.
-    IWinnersModule s_winnersModule;
-
-    /// @notice The contract implementing `IValidateMOdule` that determines that logic
-    ///     for handling validation of each follower after they are drawn during the raffle.
-    IValidateModule s_validateModule;
+    /// @notice Flag determining if raffle is already completed
+    bool public s_isRaffleFinished = false;
 
     /// @notice Event emitted when a winner is drawn from the raffle.
     event RaffleDrawn(uint256 indexed profileId);
 
     constructor(
-        ILensHub lensHub,
+        ILensHub lensHub_,
         string memory projectLensHandle,
-        uint256 numberToRaffle,
-        Randomiser randomiser,
-        IWinnersModule winnersModule,
-        IValidateModule validateModule
+        uint256 winnersToDraw_,
+        Randomiser randomiser_,
+        IWinnersModule winnersModule_,
+        IValidateModule validateModule_
     ) Ownable() {
-        s_lensHub = lensHub;
-        s_profileId = lensHub.getProfileIdByHandle(projectLensHandle);
-        s_numberToRaffle = numberToRaffle;
-        s_randomiser = randomiser;
-        s_winnersModule = winnersModule;
-        s_validateModule = validateModule;
+        lensHub = lensHub_;
+        raffleProfileId = lensHub.getProfileIdByHandle(projectLensHandle);
+        winnersToDraw = winnersToDraw_;
+        randomiser = randomiser_;
+        winnersModule = winnersModule_;
+        validateModule = validateModule_;
     }
 
     /**
-     * @notice Executes the raffle
+     * @notice Executes the raffle, picking random winners from the `ids` array.
      */
     function raffle() public {
-        DataTypes.ProfileStruct memory profile = s_lensHub.getProfile(
-            s_profileId
+        require(!s_isRaffleFinished, "Raffle already finished");
+        DataTypes.ProfileStruct memory profile = lensHub.getProfile(
+            raffleProfileId
         );
         IERC721Enumerable erc721FollowNFT = IERC721Enumerable(
             profile.followNFT
         );
         uint256 totalSupply = erc721FollowNFT.totalSupply();
-        require(totalSupply >= s_numberToRaffle);
+        require(totalSupply >= winnersToDraw, "Not enough supply for raffle");
 
-        for (uint256 i = 0; i < totalSupply; i++) {
-            ids.push(i + 1);
-        }
-
-        uint256[] memory winnerFollowerIds = new uint256[](s_numberToRaffle);
-        uint256[] memory winnerProfileIds = new uint256[](s_numberToRaffle);
+        uint256[] memory winnerProfileIds = new uint256[](winnersToDraw);
         uint256 winnerCount = 0;
-        uint256 invalidCount = 0;
-        for (uint256 i = 0; i < s_numberToRaffle; i++) {
-            uint256 randomId = getNextRandomNumber();
-            uint256 drawnId = ids[randomId];
-            uint256 drawnProfileId = s_lensHub.defaultProfile(
-                erc721FollowNFT.ownerOf(drawnId)
-            );
 
-            // Check validity of winner through module, if set
-            bool isValid = true;
-            if (address(s_validateModule) != address(0)) {
-                isValid = s_validateModule.validateFollower(drawnProfileId);
+        // Load array of registered profile IDs into mem.
+        uint256[] memory registeredIds = s_registeredIds;
+
+        bool hasWinnersModule = address(winnersModule) != address(0);
+
+        for (uint256 i = 0; i < winnersToDraw; i++) {
+            uint256 randomIndex = getNextRandomNumber() % (winnersToDraw - i);
+            uint256 drawnProfileId = s_registeredIds[randomIndex];
+
+            winnerProfileIds[winnerCount++] = drawnProfileId;
+            emit RaffleDrawn(drawnProfileId);
+
+            // Invoke winners module side-effects
+            if (hasWinnersModule) {
+                address winner = s_registeredAddresses[drawnProfileId];
+                require(winner != address(0), "Mint to zero address");
+                winnersModule.mintWhitelistToken(winner);
             }
 
-            if (isValid) {
-                // Valid winner (or no validity check)
-                emit RaffleDrawn(drawnProfileId);
-                winnerProfileIds[winnerCount] = drawnProfileId;
-                winnerFollowerIds[winnerCount] = drawnId;
-                winnerCount += 1;
-            } else {
-                // Not valid, do another run
-                invalidCount += 1;
-                i -= 1;
-                require(invalidCount <= MAX_INVALID_COUNT);
-            }
-            // Regardless of winner validity,
-            // remove ID from possible domain of IDs.
-            ids[randomId] = ids[ids.length - 1];
-            delete ids[ids.length - 1];
+            // Remove ID from possible domain of IDs, so they can't be drawn again
+            registeredIds[randomIndex] = registeredIds[
+                registeredIds.length - 1
+            ];
+            delete registeredIds[registeredIds.length - 1];
         }
 
-        if (address(s_winnersModule) != address(0)) {
-            // TODO: Change this callback function name
-            for (uint256 i = 0; i < winnerFollowerIds.length; i++) {
-                address winner = erc721FollowNFT.ownerOf(winnerFollowerIds[i]);
-                s_winnersModule.mintWhitelistToken(winner);
-            }
-        }
+        s_registeredIds = registeredIds;
+        s_isRaffleFinished = true;
     }
 
     /**
-     * @notice Get the next random number using the current seed `s_randomState`.
-     *      `s_randomState` must first be initialised.
+     * @notice Register for a raffle as the default Lens profile of the current EOA.
+     */
+    function register() external {
+        uint256 profileId = lensHub.defaultProfile(msg.sender);
+        require(
+            !s_profileIdRegistered[profileId] &&
+                !s_addressRegistered[msg.sender],
+            "Already registered for raffle"
+        );
+        // Mark profile ID & address as registered
+        s_profileIdRegistered[profileId] = true;
+        s_addressRegistered[msg.sender] = true;
+
+        // Ensure user registering for raffle passes validation
+        // This is where the validation module would check for this profile's
+        // follower count, publication count, or timestamp of follow.
+        require(
+            validateModule.validateFollower(profileId),
+            "Profile validation failed"
+        );
+        s_registeredIds.push(profileId);
+        s_registeredAddresses[profileId] = msg.sender;
+    }
+
+    /**
+     * @notice Get the next random number using current seed.
      */
     function getNextRandomNumber() private returns (uint256 randomNumber) {
-        require(s_randomState != 0);
-        randomNumber = s_randomState % s_numberToRaffle;
+        uint256 randomState = s_randomState;
+        require(randomState != 0, "Initialise random seed first");
+        randomNumber = randomState;
         s_randomState = uint256(
-            keccak256(
-                abi.encode(
-                    s_randomState,
-                    blockhash(block.number),
-                    block.difficulty
-                )
-            )
+            keccak256(abi.encode(randomState, block.difficulty))
         );
     }
 
@@ -148,7 +163,7 @@ contract Allowlister is IRandomiserCallback, Ownable {
      */
     function getRandomNumber() external onlyOwner {
         require(s_randomState == 0, "Randomness has already been set");
-        s_randomiser.getRandomNumber(address(this));
+        randomiser.getRandomNumber(address(this));
     }
 
     /**
@@ -156,7 +171,7 @@ contract Allowlister is IRandomiserCallback, Ownable {
      *      Reverts if seed has already been set.
      */
     function receiveRandomness(uint256 randomness) external {
-        require(msg.sender == address(s_randomiser));
+        require(msg.sender == address(randomiser), "Only randomiser may call");
         require(s_randomState == 0, "Randomness has already been set");
         s_randomState = randomness;
     }
--- test/index.ts
+++ test/index.ts
@@ -1,13 +1,23 @@
 import { expect } from "chai";
 import { ethers } from "hardhat";
+// eslint-disable-next-line
 import { Allowlister__factory, ILensHub__factory } from "../typechain";
 
 describe("Allowlister", () => {
-  it("should emit 'RaffleDrawn' on raffle()", async function() {
+  it("should emit 'RaffleDrawn' on raffle()", async function () {
     const [owner] = await ethers.getSigners();
-    const lensHub = ILensHub__factory.connect("0x4BF0c7AD32Fd2d32089790a54485e23f5C7736C0", owner);
-    const Allowlister = await new Allowlister__factory(owner)
-      .deploy(lensHub.address, "alice", 2, owner.address, ethers.constants.AddressZero, ethers.constants.AddressZero);
+    const lensHub = ILensHub__factory.connect(
+      "0x4BF0c7AD32Fd2d32089790a54485e23f5C7736C0",
+      owner
+    );
+    const Allowlister = await new Allowlister__factory(owner).deploy(
+      lensHub.address,
+      "alice",
+      2,
+      owner.address,
+      ethers.constants.AddressZero,
+      ethers.constants.AddressZero
+    );
     await Allowlister.deployed();
 
     await Allowlister.receiveRandomness(3);

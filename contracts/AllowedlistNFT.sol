de4f7be818a95757dca2afc6346f8ad7474ab050
zeger <zeger@equinoxai.com>
2022-04-23 16:23:14 +0200

    Supply token id

--- contracts/AllowedlistNFT.sol
+++ contracts/AllowedlistNFT.sol
@@ -1,17 +1,16 @@
 pragma solidity ^0.8.10;
 
 import "./IWinnersModule.sol";
-import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 
-contract AllowedlistNFT is ERC721, Ownable, IWinnersModule {
+contract AllowedlistNFT is ERC721Enumerable, Ownable, IWinnersModule {
 
     constructor() ERC721("AllowedlistNFT", "ANFT") Ownable() {
     }
 
     function mintWhitelistToken(address winner) external onlyOwner {
-        _safeMint(winner);
-        return true;
+        _safeMint(winner, totalSupply() + 1);
     }
 
 }
